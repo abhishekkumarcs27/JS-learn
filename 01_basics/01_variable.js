277d0d67c46df921a7da06f145e334f5a10a7b2c
ABHISHEK KUMAR <158088038+abhishekkumarcs27@users.noreply.github.com>
2025-05-16 18:22:56 +0000

    basics config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144144
+let accountEmail = "babu@gmail.com"
+var accountpassword = "1212"
+accountCity = "Delhi"
+let accountstate;
+
+
+accountEmail = "chinchan@gmail.com"
+accountpasswoed = "464846"
+accountCity = "RAIPUR"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountstate])
