401498b4c8baaca654691fa057d5f38ae905dd8e
ABHISHEK KUMAR <158088038+abhishekkumarcs27@users.noreply.github.com>
2025-05-16 19:46:39 +0000

    comparison

--- 01_basics/03_cvrnoperator.js
+++ 01_basics/03_cvrnoperator.js
@@ -28,3 +28,44 @@ console.log(stringNumber);
 console.log(typeof(stringNumber));
 
 //end
+
+//*************OPERATION*****/
+
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 3
+
+gameCounter = 100
+gameCounter++
+console.log(gameCounter);
